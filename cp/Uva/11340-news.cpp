ed423615655d506fac0d6c0cecdc78aa1040369f
Miguel <mballeng91@gmail.com>
2020-01-14 13:37:58 -0500

    news with array

--- cp/Uva/11340-news.cpp
+++ cp/Uva/11340-news.cpp
@@ -1,14 +1,28 @@
-
-#include <iostream>
-#include <string.h>
-#include <stdlib.h>
-#include <vector>
-#include <math.h>
+using namespace std;
+#include <bits/stdc++.h>
 #include <map>
-using namespace std;
-#define debug(x) cout << #x << ": " << x << "\n";
+#include<stdio.h>
+#include<string.h>
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define sqr(x) ((x)*(x))
+#define pb push_back
+#define eps 1e-9
 typedef long long ll;
-using namespace std;
+typedef unsigned long long ui64;
+typedef string st;
+typedef vector<int> vi;
+typedef vector<st> vs;
+typedef map<int,int> mii;
+typedef map<st,int> msi;
+typedef set<int> si;
+typedef set<st> ss;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+#define FOR(i, a,b) for(int i= a; i< b ; i++)
+#define FORD(i, b,a) for(int i= b; i > a ; i--)
+#define DEBUG(x) cout<<#x<<":"<<x<<"\n";
+#define MAXN  100000000000000000
 
 int main()
 {
@@ -16,14 +30,17 @@ int main()
     ll x, cnt = 0;
     char c; string l;
     scanf("%d", &n);
-    map<char, ll> mymap;
+    int data[256];
+    FOR(i, 0, 256){
+        data[i] = 0;
+    }
     for (int i = 0; i < n; ++i)
     {
         scanf("%d", &k);
         for (int j = 0; j < k; ++j)
         {
-            scanf("%s%d", &c, &x);
-            mymap[c] = x;
+            cin >> c >> x;
+            data[(int)c] = x;
         }
         scanf("%d", &m);
         cin.ignore();
@@ -32,29 +49,10 @@ int main()
             getline(cin, l);
             for (int q = 0; q < l.length(); ++q)
             {
-                ll tent = mymap[l[q]];
-                printf("tent = %f\n", tent);
-                printf("c = %c\n", l[q]);
-                if (tent != 0)
-                {
-                    cnt += tent;
-                }
+                cnt += data[(int)l[q]];
             }
         }
+        printf("%.2lf$\n", cnt / 100.0);
     }
-    string cop = to_string(cnt);
-    string dol;
-    if (cop.length() > 2)
-    {
-        for (int i = 0; i < cop.length() - 2; ++i)
-        {
-            dol += cop[i];
-        }
-        dol += '.';
-        dol += cop[cop.length() - 2];
-        dol += cop[cop.length() - 1];
-        cout << dol << "$\n";
-    } else {
-        cout <<"0."<<cnt<<"$\n";
-    }
+    return 0;
 }
