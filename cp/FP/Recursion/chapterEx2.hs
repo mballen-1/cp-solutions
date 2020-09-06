98335f79c47afdd6cf70c5124a95fd1ee4344074
Miguel Ballén <miguel.ballen@endava.com>
2020-09-06 10:34:58 -0500

    fp: Adds maybe type for dividedBy function

--- cp/FP/Recursion/chapterEx2.hs
+++ cp/FP/Recursion/chapterEx2.hs
@@ -10,4 +10,13 @@ mult x y = fn x y 0 0 where
         fn :: (Integral b) => b -> b -> b -> b -> b
         fn fs sc times acc
             | (times == sc) = acc
-            | otherwise = fn fs sc (times + 1) (acc + fs)  
+            | otherwise = fn fs sc (times + 1) (acc + fs)
+
+data DividedResult x = Result x | DividedByZero deriving Show
+
+dividedBy :: Integral a => a -> a -> (DividedResult (a,a))
+dividedBy _ 0 = DividedByZero
+dividedBy num denom = Result (go num denom 0)
+    where go n d count
+            | n < d = (count, n)
+            | otherwise = go (n - d) d (count + 1)
